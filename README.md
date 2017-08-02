f2577f2c6d5a80cee9ab7fed00ab4c8a6a247543
Artem Eroshenko <eroshenkoam@qameta.io>
2017-08-02 14:39:28 +0300

    Add aspects and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Allure TestJar Sample
+
+* `mvn clean package` - build your jar
+* `java -javaagent:target/agent/aspectjweaver.jar -jar target/allure-testjar-sample-1.0-SNAPSHOT.jar`
+* `allure serve allure-results`
--- src/main/java/com/opx/common/SimpleTest.java
+++ src/main/java/com/opx/common/SimpleTest.java
@@ -1,5 +1,6 @@
 package com.opx.common;
 
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 
 /**
@@ -10,6 +11,11 @@ public class SimpleTest {
 
     @Test
     public void simpleTest() {
+        firstStep();
+    }
+
+    @Step
+    public void firstStep() {
 
     }
 }
